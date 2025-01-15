186fc921debb7f9be75c7b1e3d8ddf2a87533e0b
IHAW <imalreadylost9@gmail.com>
2025-01-14 20:23:50 -0800

    f

--- src/App.js
+++ src/App.js
@@ -1,44 +1,64 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
+
 function App() {
   const [countriesSearch, setCountriesSearch] = useState("");
-  const [countriesData, setCountriesData] = useState([]);
-  const [filteredData,setFilteredData] = useState([]);
-  const fetchData = () => {
-    fetch("https://countriesnow.space/api/v0.1/countries")
-      .then((response) => response.json())
-      .then((result) => {
-        setCountriesData(result.data)
-        setFilteredData(result.data)
-      })
-      .catch((error) => {
-        console.log("Error", error);
-      });
+  const [filteredData, setFilteredData] = useState([]);
+  const [cities, setCities] = useState([]);
+
+  const citiesFilter = (countries) => {
+    return countries.flatMap((country) =>
+      country.cities.map((city) => `${city}, ${country.country}`)
+    );
   };
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        "https://countriesnow.space/api/v0.1/countries"
+      );
+      const result = await response.json();
+      const countriesAndCities = citiesFilter(result.data);
+      setCities(countriesAndCities);
+      setFilteredData(countriesAndCities);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  const filterData = useCallback(() => {
+    const filtered = cities
+      .filter((city) =>
+        city.toLowerCase().startsWith(countriesSearch.toLowerCase())
+      )
+      .slice(0, 5);
+    setFilteredData(filtered);
+  }, [countriesSearch, cities]);
+
+  useEffect(() => {
+    filterData();
+  }, [countriesSearch, filterData]);
+
   useEffect(() => {
     fetchData();
   }, []);
 
-  useEffect(()=>{
-    setCountriesSearch()
-  },[countriesSearch]);
-
-  const Handlechange = (event) => {
+  const handleChange = (event) => {
     setCountriesSearch(event.target.value);
   };
+
   return (
     <div className="App">
       <div>
         <input
           placeholder="Search"
-          onChange={Handlechange}
+          onChange={handleChange}
           className="w-[567px] h-[80px] border border-gray-300 rounded-[40px]"
         />
       </div>
       <div>
-        {countriesData.map((country,index)=>{
-          return <div key={index} >{country.country}</div>
-        })}
+        {countriesSearch.length > 0 &&
+          filteredData.map((city, index) => <div key={index}>{city}</div>)}
       </div>
     </div>
   );
