b84539b06657e3b07ce172c93ce1ab37eef29e95
IHAW <imalreadylost9@gmail.com>
2025-01-15 20:26:31 -0800

    s

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import "./App.css";
 import { useEffect, useState, useCallback } from "react";
+import sun from "./img/icon.png";
+import moon from "./img/icon (1).png";
+import { ReactComponent as Local_logo1 } from "./img/localization_icon (1).svg";
+import { ReactComponent as Local_logo2 } from "./img/localization_icon.svg";
+import search from "./img/search.png";
 
 function App() {
   const [countriesSearch, setCountriesSearch] = useState("");
@@ -49,16 +54,55 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <input
-          placeholder="Search"
-          onChange={handleChange}
-          className="w-[567px] h-[80px] border border-gray-300 rounded-[40px]"
-        />
-      </div>
-      <div>
-        {countriesSearch.length > 0 &&
-          filteredData.map((city, index) => <div key={index}>{city}</div>)}
+      <div className=" w-screen h-screen flex flex-row rounded-[30px] ">
+        <div className="bg-gray-300 w-2/4 h-screen rounded-[30px] flex justify-center">
+          <div className="absolute ml-[5%] mt-[5%]">
+            <img className="w-[48px] h-[48px] absolute ml-[10px] mt-[15px]" src={search} />
+            <input
+              placeholder="Search"
+              onChange={handleChange}
+              className="pl-20 w-[512px] h-[80px] rounded-[40px] font-sans text-2xl text-left text-[40px]"
+            />
+
+            <div className="bg-white border rounded-[40px]">
+              {countriesSearch.length > 0 &&
+                filteredData.map((city, index) => (
+                  <div className="py-[10px] px-[20px] text-[25px]" key={index}>
+                    {city}
+                  </div>
+                ))}
+            </div>
+          </div>
+          <div className="w-[414px] h-[828px] bg-white rounded-[48px] mt-[30%] flex flex-col items-center justify-around">
+            <p className="text-black text-[50px] ml-[10%]  font-sans font-bold tracking-[5px] flex flex-row justify-between w-[350px] p-0 items-center">
+              City <Local_logo1 />
+            </p>
+
+            <img className="w-[262px] h-[262px]" src={sun} />
+
+            <div className="flex flex-col justify-start w-[70%]">
+              <div className="text-[120px] font-[800] bg-gradient-to-t from-gray-300 to-black bg-clip-text text-transparent">
+                12
+              </div>
+              <div>Stats</div>
+            </div>
+          </div>
+        </div>
+        <div className="bg-black w-2/4 h-screen rounded-[30px] flex justify-center">
+          <div className="w-[414px] h-[828px] bg-gray-800  rounded-[48px] mt-[30%] flex flex-col items-center justify-around">
+            <p className="text-white text-[50px] ml-[10%]  font-sans font-bold tracking-[5px] flex flex-row justify-between w-[350px] p-0 items-center">
+              City <Local_logo2 />
+            </p>
+
+            <img className="w-[262px] h-[262px]" src={moon} />
+            <div className="flex flex-col justify-start w-[70%]">
+              <div className="text-[120px] font-[800] bg-gradient-to-b from-gray-300 to-black bg-clip-text text-transparent">
+                12
+              </div>
+              <div>Stats</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
